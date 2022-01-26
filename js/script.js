86708d09fd6cf12097d6d268fac177ca36c8c56d
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-01-26 11:19:30 +0100

    Header change svg to div

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var radios = document.getElementsByClassName("checkbox");
-var menuOptions = document.getElementsByClassName("menu__option");
+// var radios = document.getElementsByClassName("checkbox");
+// var menuOptions = document.getElementsByClassName("menu__option");
 
-for(var i = 0; i<radios.length; i++) {
-    radios[i].addEventListener("change", function() {
-        menuOptions[i].classList.add("opaque");
-    }) 
-}
+// for(var i = 0; i<radios.length; i++) {
+//     radios[i].addEventListener("change", function() {
+//         menuOptions[i].classList.add("opaque");
+//     }) 
+// }
