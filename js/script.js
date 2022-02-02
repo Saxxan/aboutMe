ed318cd4ee022fd88c5c925a0484e02f35000557
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-02-02 20:39:53 +0100

    Factorizado

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@
 //         menuOptions[i].classList.add("opaque");
 //     }) 
 // }
+
+
+// Función input checked
+// Coger los inputs
+// Evento checked
+// Coger los elementos a cambiar del documento
+// Darle los cambios a los elementos
